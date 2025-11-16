fbc528c4d320e6248bab03d81378a1b2e810980b
Aduragbemi_Soladoye <segunolaotan90@gmail.com>
2025-11-16 01:32:08 +0100

    Add social media handles

--- src/constants/socials.tsx
+++ src/constants/socials.tsx
@@ -1,18 +1,35 @@
 import {
   IconBrandLinkedin,
   IconBrandTwitter,
-  IconBrandYoutube,
+  IconBrandFacebook,
+  IconBrandInstagram,
+  IconBrandGithub,
 } from "@tabler/icons-react";
 
 export const socials = [
+  {
+    href: "https://www.linkedin.com/in/aduragbemi-soladoye-b3b27a395/",
+    label: "LinkedIn",
+    icon: IconBrandLinkedin,
+  },
+  {
+    href: "https://github.com/Aduragbemi350350",
+    label: "Github",
+    icon: IconBrandGithub,
+  },
   {
     href: "https://twitter.com/williams940268",
     label: "Twitter",
     icon: IconBrandTwitter,
   },
   {
-    href: "",
-    label: "LinkedIn",
-    icon: IconBrandLinkedin,
+    href: "https://www.facebook.com/profile.php?id=61560863606045",
+    label: "Facebook",
+    icon: IconBrandFacebook,
+  },
+  {
+    href: "https://www.instagram.com/oluwasegunsoladoye/",
+    label: "Instagram",
+    icon: IconBrandInstagram,
   },
 ];
